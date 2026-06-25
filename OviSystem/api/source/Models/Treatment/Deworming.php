9e161f3e665682c208a1d09d5442b33281e705a8
Enrico Garcia Alvares <cleitonferraoalvares@gmail.com>
2026-06-25 16:55:46 -0300

    Implementação das classes modelo de "Deworming" e "Disease"

--- OviSystem/api/source/Models/Treatment/Deworming.php
+++ OviSystem/api/source/Models/Treatment/Deworming.php
@@ -3,3 +3,125 @@
 namespace Source\Models\Treatment;
 use Source\Core\Model;
 use Source\Core\Connect;
+
+class Deworming extends Model
+{
+    private ?int $id;
+    private ?int $sheepsId;
+    private ?string $vermifuge;
+    private ?string $nextAplication;
+    private ?string $dose;
+    private ?string $via;
+    private ?string $aplicator;
+    private ?string $observation;
+    private ?int $active;
+
+    public function __construct(?int $id = null, ?int $sheepsId = null, ?string $vermifuge = null, ?string $nextAplication = null, ?string $dose = null, ?string $via = null, ?string $aplicator = null ,?string $observation = null,?int $active = 1)
+    {
+        $this->id = $id;
+        $this->sheepsId = $sheepsId;
+        $this->vermifuge = $vermifuge;
+        $this->nextAplication = $nextAplication;
+        $this->dose = $dose;
+        $this->via = $via;
+        $this->aplicator = $aplicator;
+        $this->observation = $observation;
+        $this->active = $active;
+
+        
+    $this->table = "deworming";
+    $this->primaryKey = "id";
+    $this->fillable = ["sheepsId", "vermifuge", "nextAplication", "dose", "via" ,"aplicator", "observation",  "active"];
+
+    }
+      public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getSheepsId(): ?int
+    {
+        return $this->sheepsId;
+    }
+
+    public function setSheepsId($sheepsId): void
+    {
+        $this->sheepsId = $sheepsId;
+    }
+
+      public function getVermifuge(): ?string
+    {
+        return $this->vermifuge;
+    }
+
+    public function setVermifuge($vermifuge): void
+    {
+        $this->vermifuge = $vermifuge;
+    }
+
+    public function getNextAplication(): ?string
+    {
+        return $this->nextAplication;
+    }
+
+    public function setNextAplication($nextAplication): void
+    {
+        $this->nextAplication = $nextAplication;
+    }
+
+    
+    public function getDose(): ?string
+    {
+        return $this->dose;
+    }
+
+    public function setDose($dose): void
+    {
+        $this->dose = $dose;
+    }
+    
+    public function getVia(): ?string
+    {
+        return $this->via;
+    }
+
+    public function setVia($via): void
+    {
+        $this->via = $via;
+    }
+
+    public function getAplicator(): ?string
+    {
+        return $this->aplicator;
+    }
+
+    public function setAplicator($aplicator): void
+    {
+        $this->aplicator = $aplicator;
+    }
+    public function getObservation(): ?string
+    {
+        return $this->observation;
+    }
+
+    public function setObservation($observation): void
+    {
+        $this->observation = $observation;
+    }
+
+    public function getActive(): ?int
+    {
+        return $this->active;
+    }
+
+    public function setActive($active): void
+    {
+        $this->active = $active;
+    }
+
+}
--- OviSystem/api/source/Models/Treatment/Disease.php
+++ OviSystem/api/source/Models/Treatment/Disease.php
@@ -3,3 +3,130 @@
 namespace Source\Models\Treatment;
 use Source\Core\Model;
 use Source\Core\Connect;
+
+class Disease extends Model
+{
+    private ?int $id;
+    private ?int $sheepsId;
+    private ?string $name;
+    private ?string $startDate;
+    private ?string $endDate;
+    private ?string $situation;
+    private ?string $veterinarian;
+    private ?string $treatment;
+    private ?string $observation;
+    private ?int $active;
+
+
+    public function __construct(?int $id = null, ?int $sheepsId = null, ?string $name = null, ?string $startDate = null, ?string $endDate = null, ?string $situation = null, ?string $veterinarian = null, ?string $treatment = null, ?string $observation = null, ?int $active = 1 )
+    {
+
+    $this->id = $id;
+    $this->sheepsId = $sheepsId;
+    $this->startDate = $startDate;
+    $this->endDate = $endDate;
+    $this->situation = $situation;
+    $this->veterinarian = $veterinarian;
+    $this->treatment = $treatment;
+    $this->observation = $observation;
+    $this->active = $active;
+
+
+    $this->table = "disease";
+    $this->primaryKey = "id";
+    $this->fillable = ["sheepsId", "startDate", "endDate", "situation", "veterinarian" ,"treatment", "observation",  "active"];
+    }
+
+
+     public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getSheepsId(): ?int
+    {
+        return $this->sheepsId;
+    }
+
+    public function setSheepsId($sheepsId): void
+    {
+        $this->sheepsId = $sheepsId;
+    }
+
+
+     public function getStartDate(): ?string
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate($startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+    public function getEndDate(): ?string
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate($endDate): void
+    {
+        $this->endDate = $endDate;
+    }
+
+    public function getSituation(): ?string
+    {
+        return $this->situation;
+    }
+
+    public function setSituation($situation): void
+    {
+        $this->situation = $situation;
+    }
+
+    public function getVeterinarian(): ?string
+    {
+        return $this->veterinarian;
+    }
+
+    public function setVeterinarian($veterinarian): void
+    {
+        $this->veterinarian = $veterinarian;
+    }
+
+    public function getTreatment(): ?string
+    {
+        return $this->treatment;
+    }
+
+    public function setTreatment($treatment): void
+    {
+        $this->treatment = $treatment;
+    }
+
+     public function getObservation(): ?string
+    {
+        return $this->observation;
+    }
+
+    public function setObservation($observation): void
+    {
+        $this->observation = $observation;
+    }
+
+    public function getActive(): ?int
+    {
+        return $this->active;
+    }
+
+    public function setActive($active): void
+    {
+        $this->active = $active;
+    }
+
+}
