6535f2e30bee78a229fa59959e169a929bd24fa5
Enrico Garcia Alvares <cleitonferraoalvares@gmail.com>
2026-09-16 15:47:06 -0300

    erro no flocks.js

--- OviSystem/views/assets/_common/classes/Flocks.js
+++ OviSystem/views/assets/_common/classes/Flocks.js
@@ -43,6 +43,6 @@ export default class Flocks{
 
 
     toJSON() {
-        return { id: this.id, userId: this.userId, name: this.userId };
+        return { id: this.id, userId: this.userId, name: this.name };
     }
 }
